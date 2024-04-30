8880bfb2bc2c06476cf191a28e940917eaf22dda
Nikita <nikitailyin186@gmail.com>
2024-05-01 00:14:09 +0300

    Finished 10th LW

--- Lab_10/PageObject/Pages/PostingAdPage.cs
+++ Lab_10/PageObject/Pages/PostingAdPage.cs
@@ -17,7 +17,7 @@ namespace Lab10
 
 
         private readonly By _descriptionFieldInAd = By.XPath("//*[@id=\"ai-form\"]/div[4]/div/div[1]/div[1]/textarea");
-        private readonly By _postAdButton = By.XPath("//*[@id=\"ai-form\"]/button");
+        private readonly By _postAdButton = By.XPath("//form[@id='ai-form']/button[text()='Подать объявление']");
         private readonly By _descriptionField = By.XPath("//form[@id='ai-form']/div[4]/p/span");
         
         public void InputDataLess20SymbolsInDescriptionField()
@@ -29,7 +29,7 @@ namespace Lab10
         }
         public void PostAnAd()
         {
-            ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 200);");
             Thread.Sleep(2000);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             wait.Until(ExpectedConditions.ElementIsVisible(_postAdButton));
